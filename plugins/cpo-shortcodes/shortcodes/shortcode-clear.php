237362538d168ffc0b914442b1ea9ada7a7e1a94
Decirée Backman <deciree.backman@deciree.eu>
2017-05-19 12:22:28 +0200

    Deleted stuff

--- plugins/cpo-shortcodes/shortcodes/shortcode-clear.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/* Clearing Shortcode */
-if(!function_exists('ctsc_shortcode_clear')){
-	function ctsc_shortcode_clear($atts, $content = null){
-		return '<div style="clear:both;width:100%;"></div>';
-	}
-	add_shortcode(ctsc_shortcode_prefix().'clear', 'ctsc_shortcode_clear');
-}
